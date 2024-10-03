31dd8258d8e2a21261c29c0de5108c9a8ac35b22
abraham <huancaaruquipaabraham@gmail.com>
2024-10-02 20:57:11 -0400

    modificado3

--- src/main/java/com/sgd/sgdfback/model/Usuario.java
+++ src/main/java/com/sgd/sgdfback/model/Usuario.java
@@ -24,6 +24,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name="user", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
 public class Usuario implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
